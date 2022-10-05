9b5556a69bb1eda8e8efac81c83e9b40125b01c8
Chioma Muofunanya <muofunanyajennifer1@gmail.com>
2022-10-05 14:17:04 +0100

    worked on an algorithm that finds the nth-rarest-item

---
+++ Nth_Rarest_Item.js
@@ -0,0 +1,18 @@
+const list = [5,4,5,4,5,4,4,5,3,3,3,2,2,1,5];
+const n = 2;
+
+function nth_most_rate_signature(list, n){
+    let numMapping = {};
+    for(let num of list){
+        if(num in numMapping){
+            numMapping[num]++; 
+        } else{
+            numMapping[num] = 1;
+        }
+        
+    }
+    for(let num in numMapping){
+        if(numMapping[num] === n) console.log(num)
+    }
+}
+nth_most_rate_signature(list, n);
